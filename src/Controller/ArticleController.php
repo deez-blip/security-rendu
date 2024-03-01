0792672f8f3f2b0317bbbce410f2be1d3dd5f36c
deez-blip <gaspard.castagnet92@gmail.com>
2024-03-01 14:01:39 +0100

    first commit

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,106 @@
+<?php
+
+// src/Controller/ArticleController.php
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/article/new", name="article_new")
+     */
+    #[Route('/article/new', name: 'article_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException('Seuls les administrateurs peuvent créer des articles.');
+        }
+
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $slug = $slugger->slug($article->getTitle()) . '-' . uniqid();
+            $article->setSlug($slug);
+
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_show', ['slug' => $article->getSlug()]);
+        }
+
+        return $this->render('article/new.html.twig', [
+            'articleForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+    #[Route('/article/{slug}', name: 'article_show')]
+    public function show(Article $article): Response
+    {
+        return $this->render('article/show.html.twig', ['article' => $article]);
+    }
+
+    /**
+     * @Route("/article/edit/{id}", name="article_edit")
+     */
+    #[Route('/article/edit/{slug}', name: 'article_edit')]
+    public function edit(Request $request, Article $article, EntityManagerInterface $entityManager): Response
+    {
+        // if (!$this->isGranted('ROLE_ADMIN')) {
+        //     throw new AccessDeniedException('Seuls les administrateurs peuvent éditer les articles.');
+        // }
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_show', ['slug' => $article->getSlug()]);
+        }
+
+        return $this->render('article/edit.html.twig', [
+            'articleForm' => $form->createView(),
+            'article' => $article
+        ]);
+    }
+
+    /**
+     * @Route("/article/delete/{id}", name="article_delete", methods={"POST"})
+     */
+    #[Route('/article/delete/{slug}', name: 'article_delete')]
+    public function delete(Request $request, Article $article, EntityManagerInterface $entityManager): Response
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException('Seuls les administrateurs peuvent supprimer les articles.');
+        }
+
+        if ($this->isCsrfTokenValid('delete' . $article->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($article);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('article_index');
+    }
+
+    #[Route('/articles', name: 'article_index')]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $articles = $entityManager->getRepository(Article::class)->findAll();
+
+        return $this->render('article/index.html.twig', ['articles' => $articles]);
+    }
+}
